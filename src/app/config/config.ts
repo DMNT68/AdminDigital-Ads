cb0a0e785319677f6bb57b1782ea6653b71856d5
Andres Salgado <andressalgado41@gmail.com>
2019-02-27 02:10:58 -0500

    Url de servicios producción

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
 // tslint:disable-next-line:eofline
-export const URL_SERVICIOS = 'http://localhost:3000';
-// export const URL_SERVICIOS = 'https://afternoon-mountain-92976.herokuapp.com';
+// export const URL_SERVICIOS = 'http://localhost:3000';
+export const URL_SERVICIOS = 'https://bckdigitalads.herokuapp.com';
